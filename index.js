4f78d6c1c3ba2e35227fe6514dbed91210a6d165
unknown <islam15-3879@diu.edu.bd>
2022-05-19 15:11:56 +0600

    manage-doctor-part-added-with-verify-admin

--- index.js
+++ index.js
@@ -38,8 +38,21 @@ async function run(){
         const serviceCollection =client.db('doctor_portal').collection('services');
         const bookingCollection =client.db('doctor_portal').collection('bookings');
         const userCollection =client.db('doctor_portal').collection('users');
+        const doctorCollection =client.db('doctor_portal').collection('doctors');
 
       
+        const verifyAdmin =async(req,res,next)=>{
+          const requester =req.decoded.email;
+          const requesterAccounter =await userCollection.findOne({email:requester});
+          if(requesterAccounter.role==='admin'){
+            next();
+          }
+          else{
+            res.status(403).send({message:'forbidden'});
+          }
+        }
+
+
         //data loadidng
         app.get('/services',async(req,res)=>{
             const query ='';
@@ -61,21 +74,17 @@ async function run(){
           res.send({admin:isAdmin})
         })
 
-        app.put('/user/admin/:email', verifyJWT, async(req,res)=>{
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin,async(req,res)=>{
           const email=req.params.email;
-          const requester =req.decoded.email;
-         const requesterAccounter =await userCollection.findOne({email:requester});
-          if(requesterAccounter.role==='admin'){
+          
             const filter ={email:email}; 
             const updateDoc = {
               $set: {role: 'admin'},
             };
-            const result =await userCollection.updateOne(filter,updateDoc);
-            res.send({result});
-          }
-          else{
-            res.status(403).send({message:'forbidden'});
-          }
+          
+          const result=await userCollection.updateOne(filter,updateDoc);
+          res.send(result);
+          
    
         });
 
@@ -138,7 +147,7 @@ async function run(){
 
 
 
-        app.get('/booking',verifyJWT, async(req,res)=>{
+        app.get('/booking',verifyJWT,verifyAdmin, async(req,res)=>{
           const patient=req.query.patient;
           const decodedEmail =req.decoded.email;
           if(patient===decodedEmail){
@@ -166,6 +175,20 @@ async function run(){
           }
           const result = await bookingCollection.insertOne(booking);
           return  res.send({success:true, result});
+        })
+
+
+        app.get('/doctor',verifyJWT,verifyAdmin ,async(req,res)=>{
+          const doctors =await doctorCollection.find().toArray();
+          res.send(doctors);
+        })
+
+
+        app.post('/doctor',verifyJWT, async(req,res)=>{
+          const doctor =req.body;
+          const result =await doctorCollection.insertOne(doctor);
+          res.send(result);
+
         })
 
         
