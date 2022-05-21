1e09c92118e27b71aac3a2c0620cb9bf1870b85a
unknown <islam15-3879@diu.edu.bd>
2022-05-21 16:27:39 +0600

    payment-client-secret-payment-added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 
@@ -51,6 +52,20 @@ async function run(){
             res.status(403).send({message:'forbidden'});
           }
         }
+
+        app.post('/create-payment-intent', verifyJWT ,async(req,res)=>{
+          const service =req.body;
+          const price =service.price;
+          const amount =price*100;
+          const paymentIntent =await stripe.paymentIntents.create({
+            amount: amount,
+            currency :'usd',
+            payment_method_types:['card']
+          });
+          res.send({clientSecret:paymentIntent.client_secret})
+
+        })
+
 
 
         //data loadidng
